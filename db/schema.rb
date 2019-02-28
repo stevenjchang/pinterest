7cdb80a8b3ab6249801c3900638460eed3a01a30
stevenjchang <stevenjchang@gmail.com>
2019-02-28 14:31:38 -0800

    add relationship, and g migration add_user_id_to_pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_222120) do
+ActiveRecord::Schema.define(version: 2019_02_28_223022) do
 
   create_table "pins", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
