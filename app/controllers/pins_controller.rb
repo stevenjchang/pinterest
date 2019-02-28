4ca7bab282bcbc7e10bcf6691c7904eb944e4635
stevenjchang <stevenjchang@gmail.com>
2019-02-28 11:32:39 -0800

    set show, find_pin, before_action show, edit, update, destroy

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,5 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
 
   def index
   end
@@ -14,10 +15,20 @@ class PinsController < ApplicationController
       redirect_to @pin, notice: "saved successfully"
     else
       render 'new'
-   end
+    end
+  end
+
+  def show
+  end
+
+  private
 
   def pin_params
     params.require(:pin).permit(:title, :description)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
